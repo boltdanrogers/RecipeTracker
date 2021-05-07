c2b471e56e60c289886f7399919f1367fedc5aee
Daniel Rogers <boldanrogers@gmail.com>
2021-05-06 20:21:47 -0400

    Add project files.

---
+++ RecipeTracker/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace RecipeTracker.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ RecipeTracker/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RecipeTracker.WebApiApplication" Language="C#" %>
