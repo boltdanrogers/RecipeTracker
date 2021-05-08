b35442a21aa761abe8cb6b17c4b2ba10b6a483f9
Jcchildr <jcchildr@gmail.com>
2021-05-08 01:46:46 -0400

    jacobs branch

---
+++ RecipeTracker.Data/IngredientList.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipeTracker.Data
+{
+    public class IngredientList
+    {
+        [Key]
+        public int IngredientListId { get; set; }
+
+        [ForeignKey (nameof(Ingredient))]
+        public string IngredientId { get; set; }
+        public virtual Ingredient Ingredient { get; set; }
+
+
+
+    }//end of class ingredientList
+}
